392eca7565fd0a2a54cd4357313217ae9202c86e
johee <josephakange@gmail.com>
2025-05-31 18:11:13 +0100

    failed implementation of dark mode

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// import { extendTheme, ThemeConfig} from "@chakra-ui/react"
+
+// const config: ThemeConfig = {
+//     intialColorMode: 'dark'
+
+// }
+
+// const theme = extendTheme({ config })
+
+// export default theme   
